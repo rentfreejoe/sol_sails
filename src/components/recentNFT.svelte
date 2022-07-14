1651ff26caf9f39af6523ff38901041790836bc6
Joe Tomlinson <joetomlinson@Joes-MBP.home>
2022-07-14 12:43:13 -0500

    yes

--- src/components/recentNFT.svelte
+++ src/components/recentNFT.svelte
@@ -1,15 +1,3 @@
-<script>
-import { listen } from "svelte/internal";
-import { onMount } from "svelte";
-const web3 = ("@solana/web3.js");
-
-(async () =>{
-const solana = new web3.Connection("https://bold-proud-meadow.solana-mainnet.quiknode.pro/10329bdaa8ea0af50ba3a95323e160f484e774f4/");
-  console.log(await solana.getLargestAccounts());
-})();
-
-</script>
-
 <p> 
 recent nft
 </p>
