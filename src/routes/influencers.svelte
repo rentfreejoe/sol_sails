c590643bd55594622cf664296d901e6ed7863fc6
Joe Tomlinson <joetomlinson@Joes-MBP.home>
2022-07-14 12:53:52 -0500

    yes

--- src/routes/influencers.svelte
+++ src/routes/influencers.svelte
@@ -5,7 +5,7 @@
 <div class="col-12">
     <h3>Wallet Watcher</h3>
 </div>
-<div class="col-3 border-radius border-green">
+<div class="col-3 border-radius border-blue">
     <div class="border-bottom">
     <img src="https://pbs.twimg.com/profile_images/1537585776216653825/JbCLCgyZ_400x400.jpg" alt="joe" class="image">
     <a class="flex-right icon" href="https://twitter.com/Rent_Free_Joe">Twitter</a>
